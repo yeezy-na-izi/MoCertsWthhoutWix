599d0fa92dc6b01226740e4d28eb68add3fd3e8c
isys35 <isys35@mail.ru>
2021-05-18 16:14:20 +0300

    add registration

--- main/forms.py
+++ main/forms.py
@@ -5,6 +5,16 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserForm(UserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs.update({'placeholder': 'Имя'})
+        self.fields['last_name'].widget.attrs.update({'placeholder': 'Фамилия'})
+        self.fields['username'].widget.attrs.update({'placeholder': 'Имя пользователя'})
+        self.fields['email'].widget.attrs.update({'placeholder': 'Email'})
+        self.fields['password1'].widget.attrs.update({'placeholder': 'Пароль'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Подтвердите пароль'})
+
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
