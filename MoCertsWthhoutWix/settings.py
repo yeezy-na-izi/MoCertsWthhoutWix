a7438983259645b96765a2bf9fcb8630c4b9e8a1
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-05-29 19:30:10 +0300

    deploy

--- MoCertsWthhoutWix/settings.py
+++ MoCertsWthhoutWix/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'MoCertsWthhoutWix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cashriser_mocert',
+        'USER': 'cashriser',
+        'PASSWORD': '1wnRa*5t',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
