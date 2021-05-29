a00138b49751f9a89c0fdc533e524a823bbb2a53
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-05-29 20:04:54 +0300

    deploy

--- MoCertsWthhoutWix/settings.py
+++ MoCertsWthhoutWix/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cashriser_mocert',
-        'USER': 'cashriser',
+        'USER': 'cashriser_mocert',
         'PASSWORD': '1wnRa*5t',
         'HOST': 'localhost',
         'PORT': '3306',
